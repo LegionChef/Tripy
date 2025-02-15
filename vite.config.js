4ed5425ab0f0b5d5591e8b863754df83986f278f
LegionChef <86323281+LegionChef@users.noreply.github.com>
2025-02-15 07:19:10 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Tripy/', // Change this to match your GitHub repository name
   plugins: [react()],
 })
+
